4a737f4177007d13ce7592c1c0381abc07d27dda
wzy <wzyzzqq@163.com>
2024-09-07 17:08:33 +0800

    feat(nest demo): nest cli基础命令练习

--- packages/demo/src/app.module.ts
+++ packages/demo/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AaaModule } from './aaa/aaa.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [AaaModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
